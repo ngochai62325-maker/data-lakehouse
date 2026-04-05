641fffae8061e9b7a2412c11252bb1090210fc92
Nguyen Hoang Son <capta1nson1304@gmail.com>
2026-04-05 10:37:07 +0700

    design whole structure of folder

---
+++ src/utils/s3_writer.py
@@ -0,0 +1,14 @@
+from src.config.settings import SILVER_PATH, GOLD_PATH
+
+
+def write_delta_table(df, layer, table_name, mode="overwrite"):
+    if layer == "silver":
+        path = f"{SILVER_PATH}/{table_name}"
+    elif layer == "gold":
+        path = f"{GOLD_PATH}/{table_name}"
+    else:
+        raise ValueError("Layer must be 'silver' or 'gold'")
+
+    df.write.format("delta").mode(mode).save(path)
+
+    print(f"Saved successfully to {path}")
